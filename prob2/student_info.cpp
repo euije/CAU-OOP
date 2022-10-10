ad05db95534af884588c3060331e246372f8d93d
euije <exitfill1234@naver.com>
2022-10-10 19:20:07 +0900

    feat: Update validation check to modularize

--- prob2/student_info.cpp
+++ prob2/student_info.cpp
@@ -3,6 +3,68 @@
 
 using namespace std;
 
+bool StudentInfo::isValidYear(string _year)
+{
+    // if _year is not 4 length string
+    if (_year.length() != 4)
+        return false;
+
+    // if _year has an empty string
+    if (_year.find(" ") != string::npos)
+        return false;
+
+    // if _year string has non-digits
+    if (!StudentInfo::isHasOnlyDigit(_year))
+        return false;
+
+    // if _year is not number && not 1900 <= _year <= 2022
+    int _year_num = stoi(_year);
+    if (!(1900 <= _year_num && _year_num <= 2022))
+        return false;
+
+    return true;
+}
+
+bool StudentInfo::isHasOnlyDigit(string number)
+{
+    /*
+        "" -> false
+        "aaaa" -> false
+        "aa11" -> false
+        "1111" -> true
+    */
+    if (number.length() == 0)
+        return false;
+
+    for (int i = 0; i < number.length(); i++)
+    {
+        if (!('0' <= number[i] && number[i] <= '9'))
+            return false;
+    }
+
+    return true;
+}
+
+bool StudentInfo::isHasOnlyAlphabet(string name)
+{
+    /*
+        "" -> false
+        "aaaa" -> true
+        "aa11" -> false
+        "1111" -> false
+    */
+    if (name.length() == 0)
+        return false;
+
+    for (int i = 0; i < name.length(); i++)
+    {
+        if (!('a' <= name[i] && name[i] <= 'z') && !('A' <= name[i] && name[i] <= 'Z'))
+            return false;
+    }
+
+    return true;
+}
+
 string StudentInfo::getName()
 {
     return this->name;
@@ -17,14 +79,116 @@ string StudentInfo::getBirthYear()
 }
 string StudentInfo::getDepartment()
 {
-    return this->deparment;
+    return this->department;
 }
 string StudentInfo::getTel()
 {
     return this->tel;
 }
 
-StudentInfo::StudentInfo(string _name, string _studentID, string _birthYear, string _department, string _tel){
+bool StudentInfo::isValidName(string _name)
+{
+    /*
+        "Name" has up to 15 (English) characters
+        "Name" should not be blank.
+    */
+
+    // if _name is blank
+    if (_name.length() == 0)
+        return false;
+
+    // string removed blank from _name
+    string nameNoSpace;
+    while (1)
+    {
+        if (_name.find(" ") != string::npos)
+        {
+            _name = _name.erase(_name.find(" "), 1);
+        }
+        else
+        {
+            nameNoSpace = _name;
+            break;
+        }
+    }
+
+    // if _name_no_space string has non-alphabet
+    if (!StudentInfo::isHasOnlyAlphabet(nameNoSpace))
+        return false;
+
+    // if nameNoSpace is longer than MAX_NAME_LEGNTH(15)
+    return nameNoSpace.length() <= MAX_NAME_LEGNTH;
+}
+
+bool StudentInfo::isValidStudentID(string _studentID)
+{
+    /*
+        "Student ID" should be exactly 10 digits
+            where first 4 digits represent addmission year.
+        "Student ID" should not be blank.
+    */
+
+    // if _studentID is blank
+    if (_studentID.length() == 0)
+        return false;
+
+    // if _studentID has an empty string
+    if (_studentID.find(" ") != string::npos)
+        return false;
+
+    // if _studentID string has no digits
+    if (!StudentInfo::isHasOnlyDigit(_studentID))
+        return false;
+
+    // if admissionYear is valid
+    if (!StudentInfo::isValidYear(_studentID.substr(0, 4)))
+        return false;
+
+    // if _studentID's length is equal to STUDENT_ID_LENGTH(10)
+    return _studentID.length() == STUDENT_ID_LENGTH;
+}
+
+bool StudentInfo::isValidBirthYear(string _birthYear)
+{
+    /*
+        "Birth Year" should be exactly 4 digits.
+        "Birth Year" is nullable
+    */
+    if(_birthYear.length() == 0)
+        return true;
+
+    return StudentInfo::isValidYear(_birthYear);
+}
+
+bool StudentInfo::isValidDepartment(string _department)
+{
+    return _department.length() <= MAX_DEPARTMENT_LENGTH;
+}
+
+bool StudentInfo::isValidTel(string _tel)
+{
+    /*
+        "Tel" has up to 12 digits
+    */
+
+    // "Tel" is nullable
+    if(_tel.length() == 0)
+        return true;
+
+    // if _tel has an empty string
+    if (_tel.find(" ") != string::npos)
+        return false;
+
+    // if _tel string has non-digits
+    if (!StudentInfo::isHasOnlyDigit(_tel))
+        return false;
+
+    // if _tel's length is not equal to MAX_TEL_LEGNTH(12)
+    return _tel.length() <= MAX_TEL_LEGNTH;
+}
+
+StudentInfo::StudentInfo(string _name, string _studentID, string _birthYear, string _department, string _tel)
+{
     this->name = _name;
     this->studentID = _studentID;
     this->birthYear = _birthYear;
@@ -34,29 +198,39 @@ StudentInfo::StudentInfo(string _name, string _studentID, string _birthYear, str
 
 /**
  * @brief Returns the result of comparison with student's field and keyword
- * 
- * @param student the instance of the information of a student
+ *
  * @param searchOption (1, name), (2, studentID), (3, studentID[0-4]), (4, department), (5, ALL)
  * @param keyword keyword given to search for specific field with searchOption
- * @return result of string.compare()
+ * @return result of string.compare() as true/false
  */
-int StudentInfo::isSameKeyword(StudentInfo student, int searchOption, string keyword){
-    if(searchOption == 1){
-        return student.getName().compare(keyword);
+bool StudentInfo::isSameKeyword(int searchOption, string keyword)
+{
+    int compareResult = -1;
+
+    if (searchOption == 1)
+    {
+        compareResult = this->getName().compare(keyword);
     }
-    else if(searchOption == 2){
-        return student.getStudentID().compare(keyword);
+    else if (searchOption == 2)
+    {
+        compareResult = this->getStudentID().compare(keyword);
     }
-    else if(searchOption == 3){
-        string admissionYear = student.getStudentID().substr(0, 4);
-        return admissionYear.compare(keyword);
+    else if (searchOption == 3)
+    {
+        string admissionYear = this->getStudentID().substr(0, 4);
+        compareResult = admissionYear.compare(keyword);
     }
-    else if(searchOption == 4){
-        return student.getDepartment().compare(keyword);
+    else if (searchOption == 4)
+    {
+        compareResult = this->getDepartment().compare(keyword);
     }
-    else if(searchOption == 5){
-        return 1;
+    else if (searchOption == 5)
+    {
+        compareResult = 0;
     }
-    
-    return 0;
+
+    if (compareResult == 0)
+        return true;
+    else
+        return false;
 }
--- prob2/student_info.h
+++ prob2/student_info.h
@@ -6,11 +6,11 @@
 
 using namespace std;
 
-#define NAME_MAX_LEGNTH 15
+#define MAX_NAME_LEGNTH 15
 #define STUDENT_ID_LENGTH 10
 #define BIRTH_YEAR_LENGTH 4
-#define DEPARTMENT_LENGTH
-#define TEL_MAX_LEGNTH 12
+#define MAX_DEPARTMENT_LENGTH 1000
+#define MAX_TEL_LEGNTH 12
 
 /**
  * @brief Class that contains information of student
@@ -29,6 +29,10 @@ private:
     string department;
     string tel;
 
+    static bool isValidYear(string _year);
+    static bool isHasOnlyDigit(string number);
+    static bool isHasOnlyAlphabet(string name);
+
 public:
     StudentInfo(string _name, string _studentID, string _birthYear, string _department, string _tel);
 
@@ -37,8 +41,14 @@ public:
     string getBirthYear();
     string getDepartment();
     string getTel();
+    
+    static bool isValidName(string _name);
+    static bool isValidStudentID(string _studentID);
+    static bool isValidBirthYear(string _birthYear);
+    static bool isValidDepartment(string _department);
+    static bool isValidTel(string _tel);
 
-    int isSameKeyword(StudentInfo student, int searchOption, string keyword);
+    bool isSameKeyword(int searchOption, string keyword);
 };
 
 #endif
