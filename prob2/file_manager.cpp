41a166e0d23d43201d37360acff7146e762d2807
euije <exitfill1234@naver.com>
2022-10-11 16:05:09 +0900

    feat: Add sort, file

--- prob2/file_manager.cpp
+++ prob2/file_manager.cpp
@@ -1,2 +1,106 @@
+#include <algorithm>
+#include <vector>
+#include <sstream>
+#include "student_info.h"
 #include "file_manager.h"
 
+void FileManager::sortInstancesWithOption(vector<StudentInfo> &instances, int option)
+{
+    if (option == 1)
+    {
+        std::sort(instances.begin(), instances.end(), StudentInfo::compareName);
+    }
+    else if (option == 2)
+    {
+        std::sort(instances.begin(), instances.end(), StudentInfo::compareStudentID);
+    }
+    else if (option == 3)
+    {
+        std::sort(instances.begin(), instances.end(), StudentInfo::compareAdmissionYear);
+    }
+    else if (option == 4)
+    {
+        std::sort(instances.begin(), instances.end(), StudentInfo::compareDepartment);
+    }
+    else
+    {
+        std::sort(instances.begin(), instances.end(), StudentInfo::compareName);
+    }
+}
+
+string FileManager::getPath()
+{
+    return this->path;
+}
+
+bool FileManager::isStreamOpen(){
+    return this->fs.is_open();
+}
+
+void FileManager::coutFileStreamError(){
+    cout << "FileStream Error: Please do openFileStream() first\n" << endl;
+}
+
+void FileManager::openFileStream(char *filename)
+{
+    while (1)
+    {
+        this->fs.open(filename);
+
+        if (this->fs.is_open())
+        {
+            cout << "You open " << filename << " file successfully!\n" << endl;
+            this->ofs.open(filename, ios::app);
+            this->path = filename;
+            break;
+        }
+        else
+        {
+            cout << filename << " doesn't exist in this directory" << endl;
+            ofstream ofs(filename);
+            if (ofs.is_open())
+            {
+                cout << filename << " file has successfully created" << endl;
+                ofs.close();
+            }
+        }
+    }
+}
+
+void FileManager::closeFileStream(){
+    this->fs.close();
+    this->ofs.close();
+}
+
+void FileManager::getStudentInstancesFromFile(vector<StudentInfo>& list){
+    if(!this->isStreamOpen()){
+        this->coutFileStreamError();
+        return;
+    }
+
+    char line[256];
+    while(this->fs.getline(line, 256)){
+        istringstream ss(line);
+        string stringBuffer;
+
+        string info[5];
+
+        for(int i = 0; getline(ss, stringBuffer, TOKEN_DIVIDER); i++){
+            info[i] = stringBuffer;
+        }
+        
+        StudentInfo student = StudentInfo(info[0], info[1], info[2], info[3], info[4]);
+        list.push_back(student);
+    }
+}
+
+void FileManager::outputStudentInfoToFile(StudentInfo student){
+    if(!this->isStreamOpen()){
+        this->coutFileStreamError();
+        return;
+    }
+
+    // this->ofs << '\n';
+    this->ofs << student.getName() + '/' + student.getStudentID() + '/' + student.getBirthYear()
+            + '/' + student.getDepartment() + '/' + student.getTel() + '\n';
+}
--- prob2/file_manager.h
+++ prob2/file_manager.h
@@ -1,8 +1,30 @@
 #ifndef FILE_MANAGER_H
 #define FILE_MANAGER_H
 
+#define TOKEN_DIVIDER '/'
+
+#include <fstream>
+#include <string>
+#include <vector>
+#include "student_info.h"
+
 class FileManager {
-    
+private:
+    string path;
+    fstream fs;
+    ofstream ofs;
+
+public:
+    string getPath();
+    bool isStreamOpen();
+    void coutFileStreamError();
+
+    void openFileStream(char* filename);
+    void closeFileStream();
+    void getStudentInstancesFromFile(vector<StudentInfo>& list);
+    void outputStudentInfoToFile(StudentInfo student);
+
+    void sortInstancesWithOption(vector<StudentInfo>& instances, int option);
 };
 
 #endif
