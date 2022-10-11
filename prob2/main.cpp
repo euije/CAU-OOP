4fc1859569d08595922af0d8bc0ece866f0cf9b4
euije <exitfill1234@naver.com>
2022-10-11 16:05:58 +0900

    feat: Update case handling

--- prob2/main.cpp
+++ prob2/main.cpp
@@ -1,68 +1,30 @@
 #include <iostream>
-#include <vector>
 #include "io_provider.h"
 #include "student_info.h"
+#include "file_manager.h"
 
 using namespace std;
 
-void TestStudentSearch()
-{
-    IOProvider provider;
-    vector<StudentInfo> students;
-
-    StudentInfo student1 = StudentInfo("Lisa Simpson", "2006303001", "Computer Engineering", "2000", "01012345678");
-    StudentInfo student2 = StudentInfo("Gildong Hong", "2004303077", "Computer Engineering", "1999", "01187651234");
-    StudentInfo student3 = StudentInfo("Euije Lee", "2019019718", "Computer Engineering", "2000", "01064082153");
-    ;
-    students.push_back(student1);
-    students.push_back(student2);
-    students.push_back(student3);
-
-    provider.outputStudentsInfoWithOption(students, 1, "Lisa Simpson");
-    provider.outputStudentsInfoWithOption(students, 2, "2019019718");
-    provider.outputStudentsInfoWithOption(students, 3, "2004");
-    provider.outputStudentsInfoWithOption(students, 4, "1999");
-    provider.outputStudentsInfoWithOption(students, 5, "");
-}
-
-void TestStudentInfoValidation()
-{
-    string input;
-
-    std::getline(cin, input);
-
-    /* private methods
-        cout << "isValidYear : " << (StudentInfo::isValidYear(input) ? "true" : "false") << endl;
-        cout << "isHasOnlyDigit : " << (StudentInfo::isHasOnlyDigit(input) ? "true" : "false") << endl;
-        cout << "isHasOnlyAlphabet : " << (StudentInfo::isHasOnlyAlphabet(input) ? "true" : "false") << '\n' << endl;
-    */
-
-    cout << "isValidName : " << (StudentInfo::isValidName(input) ? "true" : "false") << endl;
-    cout << "isValidStudentID : " << (StudentInfo::isValidStudentID(input) ? "true" : "false") << endl;
-    cout << "isValidBirthYear : " << (StudentInfo::isValidBirthYear(input) ? "true" : "false") << endl;
-    cout << "isValidDepartment : " << (StudentInfo::isValidDepartment(input) ? "true" : "false") << endl;
-    cout << "isValidTel : " << (StudentInfo::isValidTel(input) ? "true" : "false") << endl;
-}
-
 int main(int argc, char **argv)
 {
-    IOProvider provider;
+    if(argc != 2){
+        cout << "*Caution! : Please write exactly one file name" << endl;
+        return -1;
+    }
+    
+    FileManager manager;
     vector<StudentInfo> students;
+    IOProvider provider;
 
-    int mainMenuOption = -1;
-    int insertionOption;
-    int searchOption;
-    int sortingOption;
+    int mainMenuOption = 4;
+    int searchOption = 5;
+    int sortingOption = 1;
 
-    string searchKeyword;
+    string searchKeyword = "";
 
-    StudentInfo student1 = StudentInfo("Lisa Simpson", "2006303001", "2000", "Computer Engineering", "01012345678");
-    StudentInfo student2 = StudentInfo("Gildong Hong", "2004303077", "1999", "Computer Engineering", "01187651234");
-    StudentInfo student3 = StudentInfo("Euije Lee", "2019019718", "2000", "Computer Engineering", "01064082153");
-
-    students.push_back(student1);
-    students.push_back(student2);
-    students.push_back(student3);
+    manager.openFileStream(argv[1]);
+    manager.getStudentInstancesFromFile(students);
+    manager.sortInstancesWithOption(students, 1);
 
     while (1)
     {
@@ -70,6 +32,11 @@ int main(int argc, char **argv)
 
         if (mainMenuOption == 1)
         {
+            StudentInfo temp = StudentInfo("", "", "", "", "");
+            provider.IOInsertion(&temp, students);
+            students.push_back(temp);
+            manager.outputStudentInfoToFile(temp);
+            manager.sortInstancesWithOption(students, sortingOption);
         }
         else if (mainMenuOption == 2)
         {
@@ -78,10 +45,12 @@ int main(int argc, char **argv)
         }
         else if (mainMenuOption == 3)
         {
-            
+            provider.IOSorting(&sortingOption);
+            manager.sortInstancesWithOption(students, sortingOption);
         }
         else if (mainMenuOption == 4)
         {
+            manager.closeFileStream();
             break;
         }
         else
